2f5af1bdf0cd6af233392b0a7ac65d8ae24d7078
Mastan-Sk <mastansk3390@gmail.com>
2022-10-31 14:35:57 +0530

    Install Nextjs applicattion

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter 3.0</title>
+      </Head>
+
+      <main className="">
+          <h1>hello world</h1>
+      </main>
+    </div>
+  )
+}

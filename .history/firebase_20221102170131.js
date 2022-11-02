e951104c4e4708d2380fb2f67ab931d1dd5c91f3
Mastan-Sk <mastansk3390@gmail.com>
2022-11-02 17:06:33 +0530

    install next-auth and firebase and initialize the firebase

---
+++ .history/firebase_20221102170131.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp,getApp,getApps } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "twitter-3-abb79.firebaseapp.com",
+  projectId: "twitter-3-abb79",
+  storageBucket: "twitter-3-abb79.appspot.com",
+  messagingSenderId: "1055057989927",
+  appId: "1:1055057989927:web:8532bd7157516ce4395902"
+};
+
+// Initialize Firebase
+
+const app = getApps().length ? initializeApp(firebaseConfig)  : getApp() ;
+const db = getFirestore();
+const storage = getStorage();
+
+export {db,app,storage};
+
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
 
 
+
+
 export default function Home() {
   return (
     <div className="">
@@ -11,10 +13,11 @@ export default function Home() {
         <title>Twitter 3.0</title>
         <link rel='icon' href='https://img.icons8.com/arcade/64/000000/twitter.png'/>
       </Head>
-       
+ 
+         
        
       <main className='flex min-h-screen max-w-7xl mx-auto'>
-        
+
       {/* Sidebar */}
         <Sidebar/>
       {/* Feed */}
